bc0ce203d60b623fbbbe2b3cf9fce7d65aabf685
dinislam <yuldybaev.dinislam@mail.ru>
2026-05-16 12:47:20 +0000

    master: добавлен дебаг статистики ЦПУ

--- parser/stat_parser.c
+++ parser/stat_parser.c
@@ -1,10 +1,53 @@
 #include "stat_parser.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+void debug_core_stat(const struct CoreStat *stat){
+  printf("\tuser: %lu\n", stat->user);
+  printf("\tnice: %lu\n", stat->nice);
+  printf("\tsystem: %lu\n", stat->system);
+  printf("\tidle: %lu\n", stat->idle);
+  printf("\tiowait: %lu\n", stat->iowait);
+  printf("\tirq: %lu\n", stat->irq);
+  printf("\tsoftirq: %lu\n", stat->softirq);
+  printf("\tsteal: %lu\n", stat->steal);
+  printf("\tguest: %lu\n", stat->guest);
+  printf("\tguest_nice: %lu\n", stat->guest_nice);
+}
 
 void init_cpu_stat(struct CpuStat *target){
     target->cores_count = 0;
+}
+
+void debug_cpu_stat(const struct CpuStat *stat){
+  printf("Cores: %d\n", stat->cores_count);
+
+  for(int i = 0; i < stat->cores_count + 1; i++){
+    printf("Core: %d\n", i);
+    debug_core_stat(&(stat->cores_stat[i]));
+  }
+
+  printf("\n");
+}
+
+///
+/// \brief Сереализовать параметры загрузки ЦПУ в структуру
+/// \param target
+/// \param str
+///
+void parse_cpu_values(struct CoreStat *target, char *str){
+  target->user = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->nice = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->system = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->idle = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->iowait = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->irq = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->softirq = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->steal = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->guest = strtoull(strtok(NULL, " \t\n"), NULL, 10);
+  target->guest_nice = strtoull(strtok(NULL, " \t\n"), NULL, 10);
 }
 
 void stat_parser(const char *path){
@@ -18,8 +61,14 @@ void stat_parser(const char *path){
 
   while((read = getline(&line, &len, file)) != -1){
     if(strncmp(line, "cpu", 3) == 0){ // Только статистика ЦПУ
-      res.cores_count++;
-      printf("%s", line);
+      char *token = strtok(line, " \t\n");
+      if(strcmp(token, "cpu")){
+        res.cores_count++;
+      }
+
+      parse_cpu_values(&(res.cores_stat[res.cores_count]), line);
     }
   }
+
+  debug_cpu_stat(&res);
 }
--- parser/stat_parser.h
+++ parser/stat_parser.h
@@ -3,19 +3,35 @@
 
 #include <stdint.h>
 
+///
+/// \brief Структурка для статистики ядра
+///
 struct CoreStat{
-
+    uint64_t user;
+    uint64_t nice;
+    uint64_t system;
+    uint64_t idle;
+    uint64_t iowait;
+    uint64_t irq;
+    uint64_t softirq;
+    uint64_t steal;
+    uint64_t guest;
+    uint64_t guest_nice;
 };
+
+void debug_core_stat(const struct CoreStat *stat);
 
 ///
 /// \brief Структура статистики ЦПУ
 ///
 struct CpuStat{
-    int8_t cores_count;
-    struct CoreStat cores_stat[50];
+    int8_t cores_count; ///< Физическое количество ядер (без общей статистики)
+    struct CoreStat cores_stat[50]; ///< Массив структурок статистички ядер (размер равен cores_count + 1)
 };
 
 void init_cpu_stat(struct CpuStat *target);
+
+void debug_cpu_stat(const struct CpuStat *stat);
 
 ///
 /// \brief Вариант функции для передачи пути к любому файлу
